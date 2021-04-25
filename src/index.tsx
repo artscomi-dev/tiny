95d2ffa7c5e0c1b18095ccb8b6c7a3b4c501f997
artscomi <artscomi.web@gmail.com>
2021-04-25 18:01:16 +0200

    add counter and rollUp config

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react'
+import { useState, useEffect } from 'react'
 import './styles.scss'
-
-const { useState, useEffect } = React
 
 const Counter: React.FC<{
   count: number
